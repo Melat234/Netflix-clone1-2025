3c0501814aa8fb4f815f250c8859661321e5d2c5
melat234 <melatworktibebu@gmail.com>
2025-02-10 07:02:37 -0800

    axios and requests

---
+++ netflix-clone/src/utils/axios.js
@@ -0,0 +1,10 @@
+// 6fb11604dd311380ce3c021b4e6eab8d
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+// instance.get('/movie/top_rated');
+export default instance;
+
+
