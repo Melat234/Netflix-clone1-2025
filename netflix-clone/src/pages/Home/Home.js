02718605986dd596138ddeea0ae948695d222e20
melat234 <melatworktibebu@gmail.com>
2025-02-12 11:32:41 -0800

    assets

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
 import Banner from '../../Components/Banner/Banner.js'
-import RowList from '../../Components/Rows/RowList/RowList.jsx'
+import RowList from '../../Components/Rows/RowList/RowList.js'
 
 const Home = () => {
   return (
